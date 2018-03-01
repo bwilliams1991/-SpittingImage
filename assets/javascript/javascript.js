030fe9c4d96dc2c59a6eb88cad04666668e06c1c
ben <bwilly91@msn.com>
2018-02-28 22:57:55 -0700

    functional buttons, and array concat

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,10 +19,52 @@ $(document).ready(function () {
 	var searchkey = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
 
 	// Giphy API endPoint address
-	var endPoint = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchkey + "&limit=10&offset=0&rating=PG&lang=en"
+	var endPoint = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey  + "&limit=10&offset=0&rating=G&lang=en"+ "&q=";
+	
 	// Functions
 	// --------------------------------------------------------------------------------------
 	//
+
+	// onclick for all created buttons
+	$(document).on("click", ".search-value", function (value) {
+
+		
+		// console.log(value);
+		// console.log(value.target.innerText);
+		searchvalue = value.target.innerText;
+		console.log(searchvalue);
+
+		var link = endPoint + searchvalue; 
+
+		console.log("you got in");
+		// queries Giphy for 10 images then console.log() the responce
+		$.ajax({
+			url: link,
+			method: "GET"
+		}).then(function (response) {
+			console.log(response)
+
+			var results = response.data;
+
+					for (var i = 0; i < results.length; i++) {
+
+						var gifDiv = $("<div class='item'>");
+
+						var rating = results[i].rating;
+
+						var p = $("<p>").text("Rating: " + rating);
+
+						var resultsImage = $("<img>");
+
+						resultsImage.attr("src", results[i].images.fixed_height.url);
+
+						gifDiv.append(p);
+						gifDiv.append(resultsImage);
+
+						$("#results").prepend(gifDiv);
+					}
+		});
+	});
 
 	function renderButtons() {
 
@@ -31,85 +73,51 @@ $(document).ready(function () {
 		for (var i = 0; i < searchkey.length; i++) {
 
 			var buttons = $("<button>");
-	
+
 			buttons.addClass("search-value");
-	
+
 			buttons.attr("data-name", searchkey[i]);
-			
+
 			buttons.text(searchkey[i]);
-	
+
 			$("#buttons").append(buttons);
 		}
 	}
 
-	$("#submit").on("click", function(event) {
-		
+	$("#add-search").on("click", function (event) {
+		// event.preventDefault() prevents the form from trying to submit itself.
+		// We're using a form so that the user can hit enter instead of clicking the button if they want
 		event.preventDefault();
 
-	
-		var search = $("#submit").val().trim();
-
-		search = $(this).attr("data-name");
-
+		// This line will grab the text from the input box
+		var search = $("#search-input").val().trim();
+		// The search from the textbox is then added to our array
+		console.log(search);
+		console.log(this);
 		searchkey.push(search);
-
-
-console.log(searchkey);
-
+		console.log(searchkey);
+		// calling renderButtons which handles the processing of our movie array
 		renderButtons();
 	});
 
-renderButtons();
+	renderButtons();
 
 
-// queries Giphy for 10 images then console.log() the responce
-		$.ajax({
-			url: endPoint,
-			method: "GET"
-		}).then(function (response) {
-			console.log(response)
-
-			var results = response.data;
 
 
-			for (var i = 0; i < results.length; i++) {
-
-					var gifDiv = $("<div class='item'>");
-
-					var rating = results[i].rating;
-			
-					var p = $("<p>").text("Rating: " + rating);
-					
-					var resultsImage = $("<img>");
-
-					resultsImage.attr("src", results[i].images.fixed_height.url);
-
-					gifDiv.append(p);
-					gifDiv.append(resultsImage);
-
-					$("#results").prepend(gifDiv);
-			}
-		});
-	// onclick for all created buttons
-	$("button").on("click", function () {
-// what about submit???
-		
-	});
 
 	// Process
 	// --------------------------------------------------------------------------------------
 	// 
 
-	// save inputs into array
 
-	// create buttons using inputArray; re-render at each new addition
 
 	// onclick of buttons load new image(10) onto page; removing old images
 
 	// play/pause gifs
 
 	// is that too simple....
-
+	//prevent adding blank buttons
 
 
 });
